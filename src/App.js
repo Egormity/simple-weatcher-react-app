f3622f72725eb4e8be0dd4b7efa201b56b6c67c6
Egormity <kotlaregor9@gmail.com>
2024-07-02 17:31:15 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import svgSun from './images/sun.svg';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='weather-app'>
+      <SearchInput />
+      <MainCard />
+      <GenerateSmallCards amount={4} />
     </div>
   );
 }
 
-export default App;
+function SearchInput() {
+  return (
+    <div className='input-container'>
+      <input className='input-main' placeholder='Enter your city' />
+    </div>
+  );
+}
+
+function MainCard() {
+  return (
+    <div className='card-main'>
+      <img src={svgSun} />
+      <div className='text-main'>
+        <h2>Date</h2>
+        <h1 style={{ marginTop: '1rem', marginBottom: '1rem' }}>City</h1>
+        <h2>Temperature</h2>
+        <h2>Weather Condition</h2>
+      </div>
+    </div>
+  );
+}
+
+function GenerateSmallCards({ amount }) {
+  for (let i = 0; i < amount; i++) {
+    console.log('test');
+  }
+}
+
+function SmallCard() {}
+
+// `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=6557810176c36fac5f0db536711a6c52`
